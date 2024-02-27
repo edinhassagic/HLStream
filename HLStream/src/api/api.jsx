141f6ab19fc56a0a4a8f306184a42ef63d129088
Edin Hasagic <edinhassagic@gmail.com>
2024-02-27 12:51:58 +0100

    update

--- HLStream/src/api/api.jsx
+++ HLStream/src/api/api.jsx
@@ -1,21 +1,20 @@
 import axios from "axios";
 
-export const login = async ({username, password}) => {
+export const login = async ({ username, password }) => {
   try {
-    const response = await axios.get('http://localhost:4000/login', {
+    const response = await axios.get("http://localhost:4000/login", {
       headers: {
-        Authorization: `Basic ${btoa(`${username}:${password}`)}`, 
+        Authorization: `Basic ${btoa(`${username}:${password}`)}`,
         "Content-Type": "application/json",
       },
     });
 
+    localStorage.setItem("token", response.data.token);
 
-    localStorage.setItem('token', response.data.token);
-
-    return response.data.user; 
+    return response.data.user;
   } catch (error) {
-    console.error('Error during login:', error);
-    throw error; 
+    console.error("Error during login:", error);
+    throw error;
   }
 };
 
