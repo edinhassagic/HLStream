7864fa40cce22db277af173832e523d9b25bc4ac
Edin Hasagic <edinhassagic@gmail.com>
2024-02-27 13:47:04 +0100

    update

--- HLStream/src/StreamPage/StreamPage.jsx
+++ HLStream/src/StreamPage/StreamPage.jsx
@@ -1,35 +1,47 @@
 import React from "react";
-import styles from "./StreamPage.module.css"
-
-
-
-const Header = () =>  {
-
-
-
-
-
-  return(
+import styles from "./StreamPage.module.css";
+import ReactPlayer from 'react-player'
+const Header = () => {
+  return (
     <div>
-        <p>{localStorage.getItem("user")}</p>
+      <p>{localStorage.getItem("user")}</p>
+    </div>
+  );
+};
 
+
+
+
+const Stream = () => {
+  return (
+    <div>
+      <ReactPlayer  width='100%' height='400px' url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
     </div>
   )
 }
 
+const ListOfChannels = () => {
+  return (
+    <div className={styles.videolist}>
+      <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
+      <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
+      <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
+    </div>
+  )
+}
+
+
+
 const StreamPage = () => {
-
-
-  
-
   return (
     <div className={styles.layout}>
       <div className={styles.header}>
-
-        <Header/>
+        <Header />
       </div>
-      <div className={styles.stream} >stream</div>
-      <div className={styles.listOfChannels} >list of channels</div>
+      <div className={styles.stream}>
+        <Stream />
+      </div>
+      <div className={styles.listOfChannels}><ListOfChannels /></div>
     </div>
   );
 };
